98ea64b2b1f6f872a10a70345b8c4e9ad64961be
Suvineet Singh <suvineet1998@gmail.com>
2020-05-27 14:39:09 +0100

    Add time delay to automatically close door.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -19,15 +19,21 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
     
+    Owner = GetOwner();
     ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 
 void UOpenDoor::OpenDoor()
 {
-    AActor* Owner = GetOwner();
-    FRotator NewRotation = FRotator(0.f, -60.f, 0.f); // pitch, yaw, roll.
-    Owner->SetActorRotation(NewRotation);
+    // pitch, yaw, roll.
+    Owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
+}
+
+
+void UOpenDoor::CloseDoor()
+{
+    Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
 }
 
 
@@ -40,6 +46,11 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
     if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens))
     {
         OpenDoor();
+        LastDoorOpenTime = GetWorld()->GetTimeSeconds();
     }
+    
+    // Check if its time to close the door.
+    if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay)
+        CloseDoor();
 }
 
